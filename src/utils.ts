5a84624871de13b60c15b23e0427b59d087bf857
tsukiseele <me@hlo.li>
2024-11-19 19:47:02 +0800

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ const rl = readline.createInterface(stdin, stdout)
 const readPathInput = () => new Promise<string>(async (resolve, reject) => {
     do {
         const packPath = await rl.question('Enter the image directory path(or drag the directory into the window):\n')
-        let inputDir = ''
+        let inputDir = packPath
 
         if (packPath.startsWith('&')) {
             inputDir = packPath.trim().slice(3, packPath.length - 1).trim()
